5c6c7bd201526faefd9522e34e296a9643e4731c
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-06-17 15:26:46 -0300

    Primeiras views do acervo e galeria de diretores

--- centro_memoria/centro_memoria/acervo/views.py
+++ centro_memoria/centro_memoria/acervo/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from centro_memoria.instituicao.models import Instituicao
+from .models import ItemAcervo, CategoriaAcervo, FotoItemAcervo
 
-# Create your views here.
+def categorias_acervo(request):
+    instituicao = Instituicao.objects.all().order_by('-created_at')[0]
+    categorias = CategoriaAcervo.objects.filter(categoria_pai__isnull=True)
+    context = {
+        'instituicao': instituicao,
+        'categorias': categorias
+    }
+    template_name = 'categorias_acervo.html'
+    return render(request, template_name, context)
+
+def categoria_detalhes(request, categoria):
+    instituicao = Instituicao.objects.all().order_by('-created_at')[0]
+    categorias_filhas = get_object_or_404(CategoriaAcervo, categoria_pai=categoria)
+    itens_acervo = get_object_or_404(ItemAcervo, categorias__in=categoria)
+    fotos_itens_destaque = FotoItemAcervo.objects.filter(item_acervo__in=itens_acervo, destaque=True)
+    context = {
+        'instituicao': instituicao,
+        'categoria': categoria,
+        'categorias_filhas': categorias_filhas,
+        'itens_acervo': itens_acervo,
+        'fotos_itens_destaque': fotos_itens_destaque
+    }
+    template_name = 'personalidades_galeria.html'
+    return render(request, template_name, context)
+
+def item_detalhes(request, categoria, item):
+    instituicao = Instituicao.objects.all().order_by('-created_at')[0]
+    fotos_item = FotoItemAcervo.objects.filter(item_acervo=item)
+    context = {
+        'instituicao': instituicao,
+        'categoria': categoria,
+        'item': item,
+        'fotos_item': fotos_item
+    }
+    template_name = 'info_item.html'
+    return render(request, template_name, context)
--- centro_memoria/centro_memoria/galeria_diretores/views.py
+++ centro_memoria/centro_memoria/galeria_diretores/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from centro_memoria.instituicao.models import Instituicao
+from .models import Personalidade, Galeria, FotoPersonalidade
 
-# Create your views here.
+def galerias(request):
+    instituicao = Instituicao.objects.all().order_by('-created_at')[0]
+    galerias = Galeria.objects.all()
+    context = {
+        'instituicao': instituicao,
+        'galerias': galerias
+    }
+    template_name = 'galeria_index.html'
+    return render(request, template_name, context)
+
+def personalidades_galeria(request, galeria):
+    instituicao = Instituicao.objects.all().order_by('-created_at')[0]
+    personalidades = get_object_or_404(Personalidade, galeria=galeria)
+    fotos_personalidades_destaque = FotoPersonalidade.objects.filter(personalidade__in=personalidades, 
+                                                                    destaque=True)
+    context = {
+        'instituicao': instituicao,
+        'galeria': galeria,
+        'personalidades': personalidades,
+        'fotos_personalidades_destaque': fotos_personalidades_destaque
+    }
+    template_name = 'personalidades_galeria.html'
+    return render(request, template_name, context)
+
+def personalidade_detalhes(request, galeria, personalidade):
+    instituicao = Instituicao.objects.all().order_by('-created_at')[0]
+    fotos_personalidade = FotoPersonalidade.objects.filter(personalidade=personalidade)
+    context = {
+        'instituicao': instituicao,
+        'galeria': galeria,
+        'personalidade': personalidade,
+        'fotos_personalidade': fotos_personalidade
+    }
+    template_name = 'info_personalidade.html'
+    return render(request, template_name, context)
