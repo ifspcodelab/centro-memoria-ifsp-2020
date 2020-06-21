14d69fb5801bb6b2596a321ebf0b61cf8f549c1b
thomas manzi <tmsmanzi@gmail.com>
2020-06-21 15:13:57 -0300

    add funcionalidade ao procurar

--- Front-End-centro-memoria/resources/js/acervo.js
+++ Front-End-centro-memoria/resources/js/acervo.js
@@ -8,7 +8,26 @@ $(document).ready(function() {
         if (event.target != ".modal") {
             $(".modal").hide();
             $(".modal-backdrop.fade.show").hide();
+
+
         }
     }
 
 });
+
+function searchFunc() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById('ulId');
+    li = ul.getElementsByTagName('li');
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('a')[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = 'none';
+        }
+    }
+}
