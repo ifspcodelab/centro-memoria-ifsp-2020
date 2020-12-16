8d2425b75f3db8b9416ade8ab98aed39726d39f7
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-12-16 14:43:00 -0300

    Merge e alteracoes na busca avançada

--- centro_memoria/centro_memoria/acervo/forms.py
+++ centro_memoria/centro_memoria/acervo/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import CategoriaAcervo, FundoColecao, Autor
+from .models import CategoriaAcervo, FundoColecao, Autor, AtividadeEvento, TipoDocumento, TipoSuporte
 
 class DateInput(forms.DateInput):
     input_type = 'date'
@@ -17,36 +17,21 @@ class PesquisaForm(forms.Form):
 
 class PesquisaAvancadaForm(forms.Form):
 
-    CATEGORIAS = (
-        ('', 'Escolha uma categoria'),
-    )
-
-    FUNDOS_COLECOES = (
-        ('', 'Escolha...'),
-    )
-
-    AUTORES = (
-        ('', 'Escolha...'),
-    )
-
-    for categoria in CategoriaAcervo.objects.all().filter(ativo=True):
-        CATEGORIAS += ((categoria.nome, categoria.nome),)
-
-    for fundo in FundoColecao.objects.all():
-        FUNDOS_COLECOES += ((fundo.nome, fundo.nome),)
-
-    for autor in Autor.objects.all():
-        AUTORES += ((autor.nome, autor.nome),)
-
     categoria = forms.CharField(required=False, label='Categoria do acervo', widget=forms.TextInput(attrs={'class': 'form-control', 'list': 'categoriaDataList'}))
     fundo_colecao = forms.CharField(required=False, label='Fundo/Coleção', widget=forms.TextInput(attrs={'class': 'form-control', 'list': 'fundoDataList'}))
     autor = forms.CharField(required=False, label='Autor', widget=forms.TextInput(attrs={'class': 'form-control', 'list': 'autorDataList'}))
     titulo = forms.CharField(required=False, label='Título', widget=forms.TextInput(attrs={'class': 'form-control'}))
-    data = forms.DateField(required=False, label='Data', widget=DateInput(attrs={'class': 'form-control'}))
-    periodo_inicio = forms.DateField(required=False, label='De', widget=DateInput(attrs={'class': 'form-control'}))
-    periodo_fim = forms.DateField(required=False, label='até', widget=DateInput(attrs={'class': 'form-control'}))
+    descritores = forms.CharField(required=False, label='Descritores', widget=forms.TextInput(attrs={'class': 'form-control'}))
+    ano_inicio = forms.DateField(required=False, widget=DateInput(attrs={'class': 'form-control'}))
+    ano_fim = forms.DateField(required=False, widget=DateInput(attrs={'class': 'form-control'}))
+    periodo_inicio = forms.DateField(required=False, widget=DateInput(attrs={'class': 'form-control'}))
+    periodo_fim = forms.DateField(required=False, widget=DateInput(attrs={'class': 'form-control'}))
+    tipo_documento = forms.CharField(required=False, label='Tipo de Documento', widget=forms.TextInput(attrs={'class': 'form-control', 'list': 'documentoDataList'}))
+    atividade = forms.CharField(required=False, label='Atividade/Evento', widget=forms.TextInput(attrs={'class': 'form-control', 'list': 'atividadeDataList'}))
+    suporte = forms.CharField(required=False, label='Suporte', widget=forms.TextInput(attrs={'class': 'form-control', 'list': 'suporteDataList'}))
 
-    field_order = ['categoria', 'fundo_colecao', 'autor', 'titulo', 'data', 'periodo_inicio', 'periodo_fim']
+    field_order = ['categoria', 'fundo_colecao', 'autor', 'titulo', 'descritores', 'ano_inicio', 'ano_fim',
+                    'periodo_inicio', 'periodo_fim', 'tipo_documento', 'atividade', 'suporte']
     
     def save(self, commit=True):
         return {
@@ -54,7 +39,12 @@ class PesquisaAvancadaForm(forms.Form):
             'fundo_colecao': self.cleaned_data['fundo_colecao'] if self.cleaned_data['fundo_colecao'] else 'none',
             'autor': self.cleaned_data['autor'] if self.cleaned_data['autor'] else 'none',
             'titulo': self.cleaned_data['titulo'] if self.cleaned_data['titulo'] else 'none',
-            'data': self.cleaned_data['data'] if self.cleaned_data['data'] else 'none',
+            'descritores': self.cleaned_data['descritores'] if self.cleaned_data['descritores'] else 'none',
+            'ano_inicio': self.cleaned_data['ano_inicio'] if self.cleaned_data['ano_inicio'] else 'none',
+            'ano_fim': self.cleaned_data['ano_fim'] if self.cleaned_data['ano_fim'] else 'none',
             'periodo_inicio': self.cleaned_data['periodo_inicio'] if self.cleaned_data['periodo_inicio'] else 'none',
-            'periodo_fim': self.cleaned_data['periodo_fim'] if self.cleaned_data['periodo_fim'] else 'none'
+            'periodo_fim': self.cleaned_data['periodo_fim'] if self.cleaned_data['periodo_fim'] else 'none',
+            'tipo_documento': self.cleaned_data['tipo_documento'] if self.cleaned_data['tipo_documento'] else 'none',
+            'atividade': self.cleaned_data['atividade'] if self.cleaned_data['atividade'] else 'none',
+            'suporte': self.cleaned_data['suporte'] if self.cleaned_data['suporte'] else 'none'
         }
--- centro_memoria/centro_memoria/acervo/views.py
+++ centro_memoria/centro_memoria/acervo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from centro_memoria.instituicao.models import Instituicao
-from .models import ItemAcervo, CategoriaAcervo, FotoItemAcervo, DimensaoItemAcervo, Autor, FundoColecao
+from .models import (ItemAcervo, CategoriaAcervo, FotoItemAcervo, DimensaoItemAcervo, Autor, FundoColecao,
+                    AtividadeEvento, TipoSuporte, TipoDocumento)
 from django.db.models import Q
 
 import sys
@@ -39,8 +40,10 @@ def redirectPesquisa(request):
             pesquisa = form.save()
             return redirect('acervo:pesquisa_avancada', categoria=pesquisa['categoria'].lower(),
                             fundo_colecao=pesquisa['fundo_colecao'].lower(), autor=pesquisa['autor'].lower(),
-                            titulo=pesquisa['titulo'].lower(), data=pesquisa['data'],
-                            periodo_inicio=pesquisa['periodo_inicio'], periodo_fim=pesquisa['periodo_fim'])
+                            titulo=pesquisa['titulo'].lower(), ano_inicio=pesquisa['ano_inicio'], ano_fim=pesquisa['ano_fim'],
+                            periodo_inicio=pesquisa['periodo_inicio'], periodo_fim=pesquisa['periodo_fim'],
+                            tipo_documento=pesquisa['tipo_documento'].lower(), atividade=pesquisa['atividade'].lower(),
+                            suporte=pesquisa['suporte'].lower())
 
 
 def acervo(request):
@@ -56,11 +59,17 @@ def acervo(request):
     categoriasBusca = CategoriaAcervo.objects.all().filter(ativo=True)
     autoresBusca = Autor.objects.all()
     fundosBusca = FundoColecao.objects.all()
+    documentosBusca = TipoDocumento.objects.all()
+    atividadesBusca = AtividadeEvento.objects.all()
+    suportesBusca = TipoSuporte.objects.all()
 
     context = {
         'categoriasBusca': categoriasBusca,
         'autoresBusca': autoresBusca,
         'fundosBusca': fundosBusca,
+        'documentosBusca': documentosBusca,
+        'atividadesBusca': atividadesBusca,
+        'suportesBusca': suportesBusca,
         'categorias': categorias,
         'form': form,
         'formAvancado': formAvancado,
@@ -119,6 +128,9 @@ def acervo_pesquisa(request, parametro):
     categoriasBusca = CategoriaAcervo.objects.all().filter(ativo=True)
     autoresBusca = Autor.objects.all()
     fundosBusca = FundoColecao.objects.all()
+    documentosBusca = TipoDocumento.objects.all()
+    atividadesBusca = AtividadeEvento.objects.all()
+    suportesBusca = TipoSuporte.objects.all()
     categorias = CategoriaAcervo.objects.all().filter(nome__unaccent__icontains=parametro, ativo=True)
     itens_acervo = []
     if len(categorias) > 0:
@@ -131,7 +143,8 @@ def acervo_pesquisa(request, parametro):
                         Q(descricao_curta__unaccent__icontains=parametro) | Q(descricao_longa__unaccent__icontains=parametro) |
                         Q(titulo__unaccent__icontains=parametro) | Q(fundo_colecao__nome__unaccent__icontains=parametro) |
                         Q(data_inicio__icontains=parametro), ativo=True)
-        itens_acervo = itens_acervo.order_by('tipo_documento', 'data_inicio', 'titulo')
+        if (len(itens_acervo) > 0):
+            itens_acervo = itens_acervo.order_by('tipo_documento', 'data_inicio', 'titulo')
 
     categorias_pai = getCategoriasPai(itens_acervo)
 
@@ -143,6 +156,9 @@ def acervo_pesquisa(request, parametro):
         'categoriasBusca': categoriasBusca,
         'autoresBusca': autoresBusca,
         'fundosBusca': fundosBusca,
+        'documentosBusca': documentosBusca,
+        'atividadesBusca': atividadesBusca,
+        'suportesBusca': suportesBusca,
         'instituicao': instituicao,
         'itens_acervo': itens_acervo,
         'form': form,
@@ -153,7 +169,7 @@ def acervo_pesquisa(request, parametro):
     template_name = 'acervo_resultado_busca.html'
     return render(request, template_name, context)
 
-def acervo_pesquisa_avancada(request, categoria, fundo_colecao, autor, titulo, data, periodo_inicio, periodo_fim):
+def acervo_pesquisa_avancada(request, categoria, fundo_colecao, autor, titulo, ano_inicio, ano_fim, periodo_inicio, periodo_fim, tipo_documento, atividade, suporte):
     if request.method == 'POST':
         return redirectPesquisa(request)
 
@@ -163,8 +179,12 @@ def acervo_pesquisa_avancada(request, categoria, fundo_colecao, autor, titulo, d
     categoriasBusca = CategoriaAcervo.objects.all().filter(ativo=True)
     autoresBusca = Autor.objects.all()
     fundosBusca = FundoColecao.objects.all()
+    documentosBusca = TipoDocumento.objects.all()
+    atividadesBusca = AtividadeEvento.objects.all()
+    suportesBusca = TipoSuporte.objects.all()
     if ((categoria == 'none' and autor == 'none' and fundo_colecao == 'none'and titulo == 'none') and
-            (data == 'none' and periodo_inicio == 'none' and periodo_fim == 'none')):
+        (ano_inicio == 'none' and ano_fim == 'none' and periodo_inicio == 'none' and periodo_fim == 'none') and
+        (tipo_documento == 'none' and atividade == 'none' and suporte == 'none')):
         itens_acervo = []
     else:
         itens_acervo = ItemAcervo.objects.all().filter(ativo=True)
@@ -172,21 +192,39 @@ def acervo_pesquisa_avancada(request, categoria, fundo_colecao, autor, titulo, d
             categoria = CategoriaAcervo.objects.all().filter(nome__unaccent__icontains = categoria, ativo=True)
             if len(categoria) > 0:
                 itens_acervo = itens_acervo.filter(categorias__in = categoria)
+            else:
+                itens_acervo = []
         if autor != 'none':
             autores = Autor.objects.all().filter(nome__unaccent__icontains = autor)
             if len(autores) > 0:
                 itens_acervo = itens_acervo.filter(autores__in = autores)
+            else:
+                itens_acervo = []
         if fundo_colecao != 'none':
             itens_acervo = itens_acervo.filter(fundo_colecao__nome__unaccent__icontains = fundo_colecao)
         if titulo != 'none':
             itens_acervo = itens_acervo.filter(titulo__unaccent__icontains = titulo)
-        if data != 'none':
-            itens_acervo = itens_acervo.filter(data_inicio__icontains = data)
+        if ano_inicio != 'none':
+            itens_acervo = itens_acervo.filter(data_inicio__range = [ano_inicio + '-01-' + '01', '9999-12-31'])
+        if ano_fim != 'none':
+            itens_acervo = itens_acervo.filter(data_inicio__range = ['0001-01-01', ano_fim + '-01-' + '01'])
         if periodo_inicio != 'none':
             itens_acervo = itens_acervo.filter(data_inicio__range = [periodo_inicio, '9999-12-31'])
         if periodo_fim != 'none':
             itens_acervo = itens_acervo.filter(data_inicio__range = ['0001-01-01', periodo_fim])
-        itens_acervo = itens_acervo.order_by('tipo_documento', 'data_inicio', 'titulo')
+        if tipo_documento != 'none':
+            itens_acervo = itens_acervo.filter(tipo_documento__tipo__unaccent__icontains = tipo_documento)
+        if atividade != 'none':
+            atividades = AtividadeEvento.objects.all().filter(atividade__unaccent__icontains = atividade)
+            if len(atividades) > 0:
+                itens_acervo = itens_acervo.filter(atividades_eventos__in = atividades)
+            else:
+                itens_acervo = []
+        if suporte != 'none':
+            itens_acervo = itens_acervo.filter(suporte__nome__unaccent__icontains = suporte)
+
+        if (len(itens_acervo) > 0):
+            itens_acervo = itens_acervo.order_by('tipo_documento', 'data_inicio', 'titulo')
 
     categorias_pai = getCategoriasPai(itens_acervo)
 
@@ -198,6 +236,9 @@ def acervo_pesquisa_avancada(request, categoria, fundo_colecao, autor, titulo, d
         'categoriasBusca': categoriasBusca,
         'autoresBusca': autoresBusca,
         'fundosBusca': fundosBusca,
+        'documentosBusca': documentosBusca,
+        'atividadesBusca': atividadesBusca,
+        'suportesBusca': suportesBusca,
         'instituicao': instituicao,
         'itens_acervo': itens_acervo,
         'form': form,
